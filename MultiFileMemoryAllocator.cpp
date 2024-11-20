e1ed4dd2b61ce1200e850726244c3e412919fbe8
Kishan Kumar Zalavadia <kishankumarz1048@gmail.com>
2024-11-20 14:37:43 -0600

    Added a comment on how to compile the code

--- MultiFileMemoryAllocator.cpp
+++ MultiFileMemoryAllocator.cpp
@@ -202,3 +202,8 @@ int main() {
 
     return 0;
 }
+
+/* 
+  Compile the code
+  g++ -std=c++17 -o MultiFileMemoryAllocator MultiFileMemoryAllocator.cpp                                                  ─╯
+*/
