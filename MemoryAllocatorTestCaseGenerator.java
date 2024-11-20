c2c363958e637d275ba4a03b9dbc9941200e41cd
Kishan Kumar Zalavadia <kishankumarz1048@gmail.com>
2024-11-20 14:07:43 -0600

    Generate test files with no continuos C and the RL will only be called if RQ is called for a process

--- MemoryAllocatorTestCaseGenerator.java
+++ MemoryAllocatorTestCaseGenerator.java
@@ -1,7 +1,9 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Set;
 
 public class MemoryAllocatorTestCaseGenerator {
     public static void main(String[] args) {
@@ -28,6 +30,9 @@ public class MemoryAllocatorTestCaseGenerator {
                     writer.write(totalMemoryKB + "\n");
 
                     Random rand = new Random();
+                    Set<Integer> allocatedProcesses = new HashSet<>(); // Set to keep track of allocated processes
+                    String lastOperation = ""; // Track the last operation to avoid consecutive 'C'
+
                     for (int i = 1; i <= numProcesses; i++) {
                         // Randomly choose between allocation, release, and compact
                         int operation = rand.nextInt(3);
@@ -36,12 +41,25 @@ public class MemoryAllocatorTestCaseGenerator {
                             // Memory allocation request
                             int requestSize = rand.nextInt(maxMemoryRequest) + 1; // 1KB to maxMemoryRequest
                             writer.write("RQ P" + i + " " + requestSize + " " + strategy + "\n");
+                            allocatedProcesses.add(i); // Add this process to the allocated set
+                            lastOperation = "RQ"; // Update last operation
                         } else if (operation == 1) {
-                            // Memory release (randomly choose a process to release)
-                            writer.write("RL P" + rand.nextInt(i + 1) + "\n");
+                            // Memory release (randomly choose a process to release if it has been allocated memory)
+                            int processToRelease = rand.nextInt(i + 1); // Randomly choose process ID
+                            if (allocatedProcesses.contains(processToRelease)) {
+                                writer.write("RL P" + processToRelease + "\n");
+                                allocatedProcesses.remove(processToRelease); // Remove the process from the allocated set
+                                lastOperation = "RL"; // Update last operation
+                            }
                         } else {
-                            // Compact memory
-                            writer.write("C\n");
+                            // Compact memory (but not consecutively)
+                            if (!lastOperation.equals("C")) {
+                                writer.write("C\n");
+                                lastOperation = "C"; // Update last operation
+                            } else {
+                                // Skip writing the 'C' operation if the last operation was already 'C'
+                                i--; // Decrease counter to ensure we don't skip a test case
+                            }
                         }
                     }
 
@@ -63,6 +81,9 @@ public class MemoryAllocatorTestCaseGenerator {
                 writer.write(totalMemoryKB + "\n");
 
                 Random rand = new Random();
+                Set<Integer> allocatedProcesses = new HashSet<>(); // Set to keep track of allocated processes
+                String lastOperation = ""; // Track the last operation to avoid consecutive 'C'
+
                 for (int i = 1; i <= numProcesses; i++) {
                     // Randomly choose between allocation, release, and compact
                     int operation = rand.nextInt(3);
@@ -72,12 +93,25 @@ public class MemoryAllocatorTestCaseGenerator {
                         int requestSize = rand.nextInt(maxMemoryRequest) + 1; // 1KB to maxMemoryRequest
                         String strategy = getRandomStrategy(rand); // Choose random strategy (F, B, W)
                         writer.write("RQ P" + i + " " + requestSize + " " + strategy + "\n");
+                        allocatedProcesses.add(i); // Add this process to the allocated set
+                        lastOperation = "RQ"; // Update last operation
                     } else if (operation == 1) {
-                        // Memory release (randomly choose a process to release)
-                        writer.write("RL P" + rand.nextInt(i + 1) + "\n");
+                        // Memory release (randomly choose a process to release if it has been allocated memory)
+                        int processToRelease = rand.nextInt(i + 1); // Randomly choose process ID
+                        if (allocatedProcesses.contains(processToRelease)) {
+                            writer.write("RL P" + processToRelease + "\n");
+                            allocatedProcesses.remove(processToRelease); // Remove the process from the allocated set
+                            lastOperation = "RL"; // Update last operation
+                        }
                     } else {
-                        // Compact memory
-                        writer.write("C\n");
+                        // Compact memory (but not consecutively)
+                        if (!lastOperation.equals("C")) {
+                            writer.write("C\n");
+                            lastOperation = "C"; // Update last operation
+                        } else {
+                            // Skip writing the 'C' operation if the last operation was already 'C'
+                            i--; // Decrease counter to ensure we don't skip a test case
+                        }
                     }
                 }
 
